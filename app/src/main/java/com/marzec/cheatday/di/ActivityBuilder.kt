d577872ee3499ba4e6c7b38ae44a60ded4cb6eed
Mariusz Marzec <mariusz.marzec@miquido.com>
2020-05-12 11:04:55 +0200

    Fixed boot receiver

--- app/src/main/java/com/marzec/cheatday/di/ActivityBuilder.kt
+++ app/src/main/java/com/marzec/cheatday/di/ActivityBuilder.kt
@@ -3,6 +3,7 @@ package com.marzec.cheatday.di
 import com.marzec.cheatday.feature.home.MainActivity
 import com.marzec.cheatday.feature.home.MainActivityModule
 import com.marzec.cheatday.notifications.EveryDayNotificationReceiver
+import com.marzec.cheatday.receivers.BootReceiver
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -16,4 +17,8 @@ abstract class ActivityBuilder {
     @BroadcastScope
     @ContributesAndroidInjector
     abstract fun bindEveryDayNotificationReceiver(): EveryDayNotificationReceiver
+
+    @BroadcastScope
+    @ContributesAndroidInjector
+    abstract fun bindBootReceiver(): BootReceiver
 }
