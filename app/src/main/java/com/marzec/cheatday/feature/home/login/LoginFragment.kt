df67763419ee84bf21bb46bd27d463b4aa7b671a
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-01-31 08:17:24 +0100

    Added id to CurrentUser.proto, added login fragment

---
+++ app/src/main/java/com/marzec/cheatday/feature/home/login/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.marzec.cheatday.feature.home.login
+
+import com.marzec.cheatday.R
+import com.marzec.cheatday.common.BaseFragment
+
+class LoginFragment : BaseFragment(R.layout.fragment_login) {
+}
