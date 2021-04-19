8672bd2319536f033d855ac4ded179714a72f661
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-04-19 23:29:03 +0200

    Poor test config

--- app/src/main/java/com/marzec/cheatday/api/Api.kt
+++ app/src/main/java/com/marzec/cheatday/api/Api.kt
@@ -2,17 +2,22 @@ package com.marzec.cheatday.api
 
 import org.joda.time.format.DateTimeFormat
 
+
 object Api {
 
-    const val BASE_URL = "http://fiteo-env.eba-mpctrvdb.us-east-2.elasticbeanstalk.com/test/fiteo/api/1/"
+    const val env = "TEST"
 
-    const val BASE_CHEAT_URL = "http://fiteo-env.eba-mpctrvdb.us-east-2.elasticbeanstalk.com/test/cheat/api/1/"
+    val HOST = if (env == "TEST") "http://fiteo-env.eba-mpctrvdb.us-east-2.elasticbeanstalk.com/test"
+    else "http://fiteo-env.eba-mpctrvdb.us-east-2.elasticbeanstalk.com/"
+
+    val BASE_URL = "$HOST/fiteo/api/1/"
+
+    val BASE_CHEAT_URL = "$HOST/cheat/api/1/"
 
     const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
     val DATE_FORMATTER = DateTimeFormat.forPattern(DATE_FORMAT)
 
     object Headers {
-        const val env = "TEST"
 
         const val AUTHORIZATION_TEST = "Authorization-Test"
         const val AUTHORIZATION_PROD = "Authorization"
