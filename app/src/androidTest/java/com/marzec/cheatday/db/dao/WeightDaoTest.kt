fdc475dffa98efc3d03c28cdd3286ffdb3c789d4
Mariusz Marzec <mariusz.marzec00@gmail.com>
2021-05-22 13:39:47 +0200

    Weights tests works

--- app/src/androidTest/java/com/marzec/cheatday/db/dao/WeightDaoTest.kt
+++ app/src/androidTest/java/com/marzec/cheatday/db/dao/WeightDaoTest.kt
@@ -19,80 +19,79 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-//@RunWith(AndroidJUnit4::class)
-//@ExperimentalCoroutinesApi
-//class WeightDaoTest {
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class WeightDaoTest {
 
-//    private val testDispatcher = TestCoroutineDispatcher()
-//
-//    @get:Rule
-//    var instantTaskExecutorRule = InstantTaskExecutorRule()
-//
-//    private lateinit var userDao: UserDao
-//    private lateinit var weightDao: WeightDao
-//    private lateinit var db: AppDatabase
-//
-//    private val userEntity = UserEntity("uuid", "email")
-//    private val otherUser = UserEntity("other_user_uuid", "email")
-//
-//    @Before
-//    fun setUp() {
-//        val context = ApplicationProvider.getApplicationContext<Context>()
-//        db = inMemoryDatabaseBuilder(context, AppDatabase::class.java)
-//            .setTransactionExecutor(testDispatcher.asExecutor())
-//            .setQueryExecutor(testDispatcher.asExecutor())
-//            .build()
-//        userDao = db.getUserDao()
-//        weightDao = db.getWeightDao()
-//        userDao.insertCompletable(userEntity).test()
-//        userDao.insertCompletable(otherUser).test()
-//
-//    }
-//
-//    @Test
-//    fun getAllWeights(): Unit = runBlockingTest {
-//        weightDao.insert(
-//            WeightResultEntity(
-//                0,
-//                1f,
-//                1,
-//                userEntity.uuid
-//            )
-//        )
-//        weightDao.insert(
-//            WeightResultEntity(
-//                0,
-//                1f,
-//                0,
-//                userEntity.uuid
-//            )
-//        )
-//        weightDao.insert(
-//            WeightResultEntity(
-//                0,
-//                1f,
-//                0,
-//                otherUser.uuid
-//            )
-//        )
-//        val actualItems = weightDao.observeWeights(userEntity.uuid).first()
-//            .toList()
-//        Assert.assertEquals(
-//            listOf(
-//                WeightResultEntity(
-//                    1,
-//                    1f,
-//                    1,
-//                    userEntity.uuid
-//                ),
-//                WeightResultEntity(
-//                    2,
-//                    1f,
-//                    0,
-//                    userEntity.uuid
-//                )
-//            ),
-//            actualItems
-//        )
-//    }
-//}
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var userDao: UserDao
+    private lateinit var weightDao: WeightDao
+    private lateinit var db: AppDatabase
+
+    private val userEntity = UserEntity("uuid", "email")
+    private val otherUser = UserEntity("other_user_uuid", "email")
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            .setTransactionExecutor(testDispatcher.asExecutor())
+            .setQueryExecutor(testDispatcher.asExecutor())
+            .build()
+        userDao = db.getUserDao()
+        weightDao = db.getWeightDao()
+        userDao.insert(userEntity)
+        userDao.insert(otherUser)
+    }
+
+    @Test
+    fun getAllWeights(): Unit = runBlockingTest {
+        weightDao.insert(
+            WeightResultEntity(
+                0,
+                1f,
+                1,
+                userEntity.uuid
+            )
+        )
+        weightDao.insert(
+            WeightResultEntity(
+                0,
+                1f,
+                0,
+                userEntity.uuid
+            )
+        )
+        weightDao.insert(
+            WeightResultEntity(
+                0,
+                1f,
+                0,
+                otherUser.uuid
+            )
+        )
+        val actualItems = weightDao.observeWeights(userEntity.uuid).first()
+            .toList()
+        Assert.assertEquals(
+            listOf(
+                WeightResultEntity(
+                    1,
+                    1f,
+                    1,
+                    userEntity.uuid
+                ),
+                WeightResultEntity(
+                    2,
+                    1f,
+                    0,
+                    userEntity.uuid
+                )
+            ),
+            actualItems
+        )
+    }
+}
