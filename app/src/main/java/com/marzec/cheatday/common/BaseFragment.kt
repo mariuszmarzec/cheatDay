599d6c75834bde2be171346daaec05a5eddc2f50
Mariusz Marzec <mariusz.marzec00@gmail.com>
2024-01-28 13:25:48 +0100

    Fixed observing flows in lifecycle aware

--- app/src/main/java/com/marzec/cheatday/common/BaseFragment.kt
+++ app/src/main/java/com/marzec/cheatday/common/BaseFragment.kt
@@ -2,9 +2,11 @@ package com.marzec.cheatday.common
 
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 abstract class BaseFragment : Fragment {
 
@@ -13,9 +15,11 @@ abstract class BaseFragment : Fragment {
     constructor(@LayoutRes contentLayoutId: Int) : super(contentLayoutId)
 
     inline fun <T> Flow<T>.observeOnResume(crossinline action: (value: T) -> Unit) {
-        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
-            collect {
-                action(it)
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                collect {
+                    action(it)
+                }
             }
         }
     }
