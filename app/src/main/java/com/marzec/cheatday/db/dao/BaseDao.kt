6ff090da5dd6286dfd283a5d7b39058e665a76d8
Mariusz Marzec <mariusz.marzec00@gmail.com>
2024-03-30 20:55:46 +0100

    Added upsert

--- app/src/main/java/com/marzec/cheatday/db/dao/BaseDao.kt
+++ app/src/main/java/com/marzec/cheatday/db/dao/BaseDao.kt
@@ -3,6 +3,7 @@ package com.marzec.cheatday.db.dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
+import androidx.room.Upsert
 import com.marzec.cheatday.db.model.db.WeightResultEntity
 
 interface BaseDao<T> {
@@ -15,7 +16,10 @@ interface BaseDao<T> {
 
     @Update
     fun update(ob: T)
-    
+
+    @Upsert
+    fun upsert(ob: T)
+
     @Insert
     suspend fun insertSuspend(ob: T)
 
