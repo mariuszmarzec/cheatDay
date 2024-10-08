c014be087e6be6c095ee3a8ac8328042972f80ad
Mariusz Marzec <mariusz.marzec00@gmail.com>
2024-10-08 23:51:28 +0200

    Added jacoco verification

--- app/jacoco.gradle.kts
+++ app/jacoco.gradle.kts
@@ -34,3 +34,30 @@ tasks.create("jacocoTestReportStageDebug", JacocoReport::class) {
         }
     )
 }
+
+
+tasks.create("jacocoTestCoverageVerification", JacocoCoverageVerification::class) {
+    dependsOn("jacocoTestReportStageDebug")
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.5".toBigDecimal()
+            }
+        }
+
+        rule {
+            isEnabled = false
+            element = "CLASS"
+            includes = listOf("org.gradle.*")
+
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "0.3".toBigDecimal()
+            }
+        }
+    }
+}
+
+
